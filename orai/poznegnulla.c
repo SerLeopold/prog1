984792354f86f9ce221b86e9bdec14f19ac7e696
Botond Büscsei <79343488+SerLeopold@users.noreply.github.com>
2025-02-28 10:25:13 +0100

    Create poznegnulla.c

---
+++ orai/poznegnulla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main ()
+{
+    int input;
+    printf("Egész szám: ");
+    scanf("%d", &input);
+
+    if(input>0)
+        {
+        printf("Pozitiv\n");
+        }
+    else if (input<0)
+        {
+        printf("Negativ\n");
+        }
+    else 
+        {
+        printf("Nulla\n");
+        }
+
+    return 0;
+    
+    
+    }
