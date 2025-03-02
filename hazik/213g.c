eef9a2d27d5292eee7fa7258e57f395ba7432ddc
Botond Büscsei <79343488+SerLeopold@users.noreply.github.com>
2025-03-02 19:40:53 +0100

    Create 213g.c

---
+++ hazik/213g.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int szam = 0;
+    
+
+    for (int i=1; i<=100; i++)
+    {
+        szam +=i;
+        printf("Az összeg: %d\n", szam);
+
+    }
+
+    return 0;
+}
