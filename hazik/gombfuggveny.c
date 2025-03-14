1e5f37e2addc29059f880b1a178cc8bb61f91763
Botond Büscsei <79343488+SerLeopold@users.noreply.github.com>
2025-03-14 10:04:58 +0100

    Update gombfuggveny.c

--- hazik/gombfuggveny.c
+++ hazik/gombfuggveny.c
@@ -2,7 +2,7 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-// Függvények deklarációja
+
 double get_double(const char* szoveg);
 double gomb_felszin(double sugar);
 double gomb_terfogat(double sugar);
@@ -10,14 +10,13 @@ double gomb_terfogat(double sugar);
 int main() {
     double sugar = get_double("Add meg a gömb sugarát: ");
     
-    // Felszín és térfogat kiszámítása és kiírása
+    
     printf("A gömb felszíne: %.2f m\n", gomb_felszin(sugar));
     printf("A gömb térfogata: %.2f m^2\n", gomb_terfogat(sugar));
     
     return 0;
 }
 
-// Függvény a valós szám bekérésére
 double get_double(const char* szoveg) {
     double szam;
     printf("%s", szoveg);
@@ -25,12 +24,11 @@ double get_double(const char* szoveg) {
     return szam;
 }
 
-// Gömb felszínének kiszámítása
+
 double gomb_felszin(double sugar) {
     return 4 * M_PI * sugar * sugar;
 }
 
-// Gömb térfogatának kiszámítása
 double gomb_terfogat(double sugar) {
     return (4.0 / 3.0) * M_PI * pow(sugar, 3);
 }
