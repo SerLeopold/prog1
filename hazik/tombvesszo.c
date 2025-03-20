cc1160f6cb00c298c7e20f99353e56bdfef69a7e
Botond Büscsei <79343488+SerLeopold@users.noreply.github.com>
2025-03-20 13:22:33 +0100

    Create tombvesszo.c

---
+++ hazik/tombvesszo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void print_array(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (i != size - 1) {
+            printf("%d, ", arr[i]);
+        } else {
+            printf("%d\n", arr[i]);
+        }
+    }
+}
+
+int main() {
+    int arr[] = {4, 7, 34, 23, 67};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    print_array(arr, size);
+
+    return 0;
+}
