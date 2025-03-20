5340e6836ae705d5bdfba1834ee6cc7569e7dfc2
Botond Büscsei <79343488+SerLeopold@users.noreply.github.com>
2025-03-20 19:53:32 +0100

    Update tombvesszo.c

--- hazik/tombvesszo.c
+++ hazik/tombvesszo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void print_tomb(int arr[], int size) {
+void tomb_kiir(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         if (i != size - 1) {
             printf("%d, ", arr[i]);
@@ -14,7 +14,7 @@ int main() {
     int arr[] = {4, 7, 34, 23, 67};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    print_tomb(arr, size);
+    tomb_kiir(arr, size);
 
     return 0;
 }
