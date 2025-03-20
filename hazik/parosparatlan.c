6e38f167aa94bd0ddb75d2141cc58a65ba9bf318
Botond Büscsei <79343488+SerLeopold@users.noreply.github.com>
2025-03-20 19:13:12 +0100

    Update parosparatlan.c

--- hazik/parosparatlan.c
+++ hazik/parosparatlan.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int is_even(int n) {
     if (n % 2 == 0) {
         return 1;
@@ -6,8 +8,20 @@ int is_even(int n) {
     }
 }
 
-// b.)
-
 int is_odd(int n) {
     return !is_even(n);
+}
+
+int main() {
+    int num;
+    printf("Adj meg egy számot: ");
+    scanf("%d", &num);
+
+    if (is_even(num)) {
+        printf("%d páros.\n", num);
+    } else {
+        printf("%d páratlan.\n", num);
+    }
+    
+    return 0;
 }
