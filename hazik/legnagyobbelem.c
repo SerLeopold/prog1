8f0af455fd13b608d5e5bd561b666eaaff0bbdd7
Botond Büscsei <79343488+SerLeopold@users.noreply.github.com>
2025-03-20 19:18:34 +0100

    Update legnagyobbelem.c

--- hazik/legnagyobbelem.c
+++ hazik/legnagyobbelem.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int find_max(int arr[], int size) {
+int max_keres(int arr[], int size) {
     int max = arr[0];
     for (int i = 1; i < size; i++) {
         if (arr[i] > max) {
@@ -14,7 +14,7 @@ int main() {
     int arr[] = {5, 3, 8, 1, 6};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    printf("A legnagyobb elem: %d\n", find_max(arr, size));
+    printf("A legnagyobb elem: %d\n", max_keres(arr, size));
 
     return 0;
 }
