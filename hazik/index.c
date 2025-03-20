1584fb3f4615fe266523080c18900283c3bce9bc
Botond Büscsei <79343488+SerLeopold@users.noreply.github.com>
2025-03-20 19:36:55 +0100

    Update index.c

--- hazik/index.c
+++ hazik/index.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int index(int arr[], int size, int value) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == value) {
-            return i;
-        }
+int sum_magassagkulonbseg(const char *num_str) {
+    int sum = 0;
+    for (int i = 0; num_str[i + 1] != '\0'; i++) {
+        sum += abs((num_str[i] - '0') - (num_str[i + 1] - '0'));
     }
-    return -1;
+    return sum;
 }
 
 int main() {
-    int arr[] = {5, 3, 8, 1, 6};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int value = 3;
-
-    int result = index(arr, size, value);
+    char num_str[] = "17976931348623159077293051907890247336179769789423065727343008115"
+                     "77326758055009631327084773224075360211201138798713933576587897688"
+                     "14416622492847430639474124377767893424865485276302219601246094119"
+                     "45308295208500576883815068234246288147391311054082723716335051068"
+                     "4586298239947245938479716304835356329624224137216";
     
-    if (result != -1) {
-        printf("Az elem megtalálható a(z) %d. indexen.\n", result);
-    } else {
-        printf("Az elem nem szerepel a tombben.\n");
-    }
-
+    int result = sum_magassagkulonbseg(num_str);
+    printf("A magasságkülönbségek összege: %d\n", result);
+    
     return 0;
 }
