6715fc9cf7433e54f5bf38ffeb5e9478b923e6ff
Botond Büscsei <79343488+SerLeopold@users.noreply.github.com>
2025-03-20 19:38:44 +0100

    Update felhokarcolok.c

--- hazik/felhokarcolok.c
+++ hazik/felhokarcolok.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int calculate_difference_sum(int arr[], int size) {
+int sum_magassagkulonbseg(const char *num_str) {
     int sum = 0;
-    for (int i = 0; i < size - 1; i++) {
-        sum += abs(arr[i] - arr[i + 1]);
+    for (int i = 0; num_str[i + 1] != '\0'; i++) {
+        sum += abs((num_str[i] - '0') - (num_str[i + 1] - '0'));
     }
     return sum;
 }
 
 int main() {
-    int num = 21024;
-    int arr[5];
-    int size = 0;
-
-    while (num > 0) {
-        arr[size++] = num % 10;
-        num /= 10;
-    }
-
-    for (int i = 0; i < size / 2; i++) {
-        int temp = arr[i];
-        arr[i] = arr[size - i - 1];
-        arr[size - i - 1] = temp;
-    }
-
-    int result = calculate_difference_sum(arr, size);
-
-    printf("A szomszedos felhokarcolok magassagkulonbsegenek osszege: %d\n", result);
-
+    char num_str[] = "17976931348623159077293051907890247336179769789423065727343008115"
+                     "77326758055009631327084773224075360211201138798713933576587897688"
+                     "14416622492847430639474124377767893424865485276302219601246094119"
+                     "45308295208500576883815068234246288147391311054082723716335051068"
+                     "4586298239947245938479716304835356329624224137216";
+    
+    int result = sum_magassagkulonbseg(num_str);
+    printf("A magasságkülönbségek összege: %d\n", result);
+    
     return 0;
 }
