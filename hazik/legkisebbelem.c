186e3fde5d4f08e1db0e8ea8813333780b0584af
Botond Büscsei <79343488+SerLeopold@users.noreply.github.com>
2025-03-20 19:17:13 +0100

    Update legkisebbelem.c

--- hazik/legkisebbelem.c
+++ hazik/legkisebbelem.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int find_min(int arr[], int size) {
+int min_keres(int arr[], int size) {
     int min = arr[0];
     for (int i = 1; i < size; i++) {
         if (arr[i] < min) {
@@ -14,7 +14,7 @@ int main() {
     int arr[] = {5, 3, 8, 1, 6};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    printf("A legkisebb elem: %d\n", find_min(arr, size));
+    printf("A legkisebb elem: %d\n", min_keres(arr, size));
     
     return 0;
 }
