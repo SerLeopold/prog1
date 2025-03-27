8436ab0228a19f591d539e5a45dfe8733acc0ca9
Botond Büscsei <79343488+SerLeopold@users.noreply.github.com>
2025-03-28 00:05:47 +0100

    Update 2025.c

--- hazik/2025.c
+++ hazik/2025.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int main() {
+   
     char a = 'B'-'A';
     char b = a+a;
     char c = b+b+b+b+b;
     int d = (c*c)*(b*c)+(c*b)+(a+a+a+a+a);
     printf("%d\n", d);
+
+
+    
     return 0;
 }
